4d930a71f61d27d43f0c2c6ff6423dee6260622f
Stone98 <stone18003@gmail.com>
2021-04-23 12:17:56 -0700

    auth test passing

--- api/server.test.js
+++ api/server.test.js
@@ -18,3 +18,42 @@ afterAll(async () => {
 test("sanity", () => {
   expect(true).toBe(true);
 });
+
+describe("server.js", () => {
+  describe("[POST] /api/auth/register - creating user", () => {
+    it("[1] creates a new user in the database", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "foobar", password: "1234" });
+      const foobar = await db("users").where("username", "foobar").first();
+      expect(foobar).toMatchObject({ username: "foobar" });
+    });
+    it("[2] responds with proper status on succcess", async () => {
+      const res = await request(server)
+        .post("/api/auth/register")
+        .send({ username: "foobar", password: "1234" });
+      expect(res.status).toBe(201);
+    });
+  });
+  describe("[POST] /api/auth/login", () => {
+    it("[3] responds with the correct message on valid credentials", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "foobar", password: "1234" });
+      const res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "foobar", password: "1234" });
+      expect(res.body.message).toMatch(/welcome, foobar/i);
+    });
+    it("[4] responds with the correct message on invalid credentials", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "foobar", password: "1234" });
+      const res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "foo", password: "1234" });
+      expect(res.body.message).toMatch(/Invalid Credentials/i);
+      expect(res.status).toBe(401);
+    });
+  });
+});
